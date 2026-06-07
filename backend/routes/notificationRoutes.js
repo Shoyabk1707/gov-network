f4411aec43a959f10b88ad32f10d98ce203bcb03
Shoyabk1707 <shoyabcse201@gmail.com>
2026-06-07 16:45:59 +0530

    Correction in routes/NotificationRoute.js

--- backend/routes/notificationRoutes.js
+++ backend/routes/notificationRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { getNotifications } = require('../controllers/notificationController');
-const auth = require('../middleware/auth');
+const auth = require('../middleware/authMiddleware');
 
 router.get('/', auth, getNotifications);
 module.exports = router;
